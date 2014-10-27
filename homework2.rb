14b2312f20c822bf0b326f66a28b87ab2884eb1f
Emily DuBois <emilymdubois@gmail.com>
2014-10-27 17:04:35 -0400

    Workable code for minimal requirements

--- homework2.rb
+++ homework2.rb
@@ -2,9 +2,7 @@ def get_answer
 	menu = ["create", "view", "delete", "quit"]
 	entry = ""
 	while not menu.include? entry do
-		puts "\n\nWelcome to your address book main menu!\n
-What would you like to do?\n
-Enter \"create\" to create a new entry
+		puts "\nEnter \"create\" to create a new entry
 Enter \"view\" to view an existing entry
 Enter \"delete\" to delete an existing entry
 Enter \"quit\" to quit the application\n\n"
@@ -17,12 +15,12 @@ Enter \"quit\" to quit the application\n\n"
 	end
 end
 
-def assign_answer
-	address_book = []
+def assign_answer (a)
 	menu_entry = get_answer
-
+	
 	if menu_entry == "create"
-		def new_entry
+
+		new_entry = lambda do
 			address_book_temp = []
 			puts "\nYou are adding a new entry.\n\n"
 			print "What is the first name? "
@@ -31,7 +29,7 @@ def assign_answer
 			print "What is the last name? "
 			last_name = gets.chomp
 			address_book_temp.push last_name
-			print "What is the phone number (xxx-xxx-xxxx)? "
+			print "What is the phone number? "
 			phone_number = gets.chomp
 			address_book_temp.push phone_number
 			print "What is the email address? "
@@ -40,35 +38,95 @@ def assign_answer
 			puts "\nYou successfully added a contact you to your address book!\n\n"
 			return address_book_temp
 		end
-		address_book << new_entry
-		puts address_book.inspect
+
+		a << new_entry.call
+		puts a
+
 		loop do
-			assign_answer
+			assign_answer(a)
 			break if get_answer == "quit"
 		end
 	
 	elsif menu_entry == "view"
 
-		def print_address_book (x)
+		retrieve_entry = lambda do
 			puts "\n**************************************"
-			x.length.times do |index|
-				print index," ",x[index][1],", ",x[index][0]
-				puts""
-			end
+				a.length.times do |index|
+					print index," ",a[index][1],", ",a[index][0]
+					puts""
+				end
 			puts "**************************************"
+			print "\nWhich entry would you like to view (enter \"9999\" to return to main menu)? "
+			index = gets.chomp.to_i
+			if a.empty?
+				puts "\nWhoops it looks like the array is empty!\nRedirecting you to the main menu..."
+				loop do
+					assign_answer(a)
+					break if get_answer == "quit"
+				end
+			elsif index == 9999
+				puts "\nRedirecting you to the main menu..."
+				loop do
+					assign_answer(a)
+					break if get_answer == "quit"
+				end
+			elsif index >= 0 and index <= a.length-1
+				puts ""
+				print a[index][1],", ",a[index][0],"\n",a[index][2],"\n",a[index][3]
+				puts ""
+			else
+				puts "\nSorry, that's an invalid entry! Try again."
+				puts retrieve_entry.call
+			end
 		end
 
-		### Note this address book info is filler until I can figure out how to keep the array populated ###
-		address_book = [["Emily", "DuBois", "512-466-5165", "emilymdubois@gmail.com"],["Alex", "Ford", "555-555-5555", "alexford@gmail.com"]]
-		y = print_address_book(address_book)
-
+		retrieve_entry.call
 		loop do
-			assign_answer
+			assign_answer(a)
 			break if get_answer == "quit"
 		end
 	
 	elsif menu_entry == "delete"
-		puts "Delete an existing entry"
+		
+		delete_entry = lambda do
+			puts "\n**************************************"
+				a.length.times do |index|
+					print index," ",a[index][1],", ",a[index][0]
+					puts""
+				end
+			puts "**************************************"
+			print "\nWhich entry would you like to delete (enter \"9999\" to return to main menu)? "
+			index = gets.chomp.to_i
+			if a.empty?
+				puts "\nWhoops it looks like the array is empty!\nRedirecting you to the main menu..."
+				loop do
+					assign_answer(a)
+					break if get_answer == "quit"
+				end
+			elsif index == 9999
+				puts "\nRedirecting you to the main menu..."
+				loop do
+					assign_answer(a)
+					break if get_answer == "quit"
+				end
+			elsif index >= 0 and index <= a.length-1
+				puts ""
+				print "Now deleting #{a[index][0]} #{a[index][1]}..."
+				puts ""
+				a.delete_at index-1
+			else
+				puts "\nSorry, that's an invalid entry! Try again."
+				puts delete_entry.call
+			end
+		end
+
+		puts "\nHere is your updated address book:\n\n"
+		delete_entry.call
+
+		loop do
+			assign_answer(a)
+			break if get_answer == "quit"
+		end
 	
 	elsif menu_entry == "quit"
 		puts "\nGoodbye!"
@@ -76,6 +134,9 @@ def assign_answer
 		exit
 	
 	end
+
 end
 
-puts assign_answer
+address_book = []
+puts "\nWelcome to your address book main menu!\n\nWhat would you like to do?"
+puts assign_answer(address_book)
